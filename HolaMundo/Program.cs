32abcf1cd589d45524e2debb3064b2f7eb7fa8e8
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2024-02-02 09:50:35 +0100

    Add project files.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1- Pedir mi nombre
+            Console.WriteLine("Escribe tu nombre");
+            string? nombre = Console.ReadLine();
+
+            // 2- Obtener la suma de dos números
+            double suma = Suma(5, 6);
+
+            // 3- Mostrar Hola Mundo, mi nombre y la suma
+            Console.WriteLine("Hola Mundo!!!!!");
+            Console.WriteLine($"Mi nombre es {nombre}");
+            Console.WriteLine($"La suma es {suma}");
+        }
+
+        public static int Suma(int b, int a)
+        {
+            return a + b;
+        }
+    }
+
+}
