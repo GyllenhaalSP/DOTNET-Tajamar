3349251b8354a794a4768b9e7e062988a12dc60f
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2024-02-09 12:30:08 +0100

    Excepciones y excepciones personalizadas.

---
+++ Excepcion/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Excepcion;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var lectorArchivo = new LectorArchivo();
+
+        try
+        {
+            lectorArchivo.LeerArchivo(@"C:\Users\wizar\Downloads\Ejempldo.txt");
+        }
+        catch (ExcepcionPersonalizada e)
+        {
+            Console.WriteLine($"Mensajito: {e.Message}, Fecha: {e.FechaHoraExcepcion}, Ruta: {e.Archivo}");
+        }
+    }
+}
