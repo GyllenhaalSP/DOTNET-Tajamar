958889d7fb54d7f3049b80f51e7eb1b36faf9c25
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2024-02-14 13:15:30 +0100

    Ejemplo de interfaces con Pagos.

---
+++ Pagos/IPago.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pagos
+{
+    public interface IPago
+    {
+        string Servicio { get; }
+        bool ProcesarPago(decimal total);
+        bool VerificarPago();
+    }
+}
---
+++ Pagos/PagoConPayPal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pagos
+{
+    public class PagoConPayPal : IPago
+    {
+        public string Servicio => "PayPal";
+        public bool ProcesarPago(decimal total)
+        {
+            Console.WriteLine($"Procesando pago con PayPal por importe de {total:C}");
+            return true;
+        }
+
+        public bool VerificarPago()
+        {
+            Console.WriteLine("Verificando pago con PayPal");
+            return true;
+        }
+    }
+}
