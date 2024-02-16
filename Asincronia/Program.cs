3ee1b474fee003a9ea7958b9828570e6e3624484
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2024-02-16 12:03:32 +0100

    Modificada app escritorio.

--- Asincronia/Program.cs
+++ Asincronia/Program.cs
@@ -14,21 +14,21 @@ internal class Program
     static async Task OperacionLargaDuracionA()
     {
         Console.WriteLine("Iniciando operación de larga duración...");
-        await Task.Delay(3000); // Simula una operación que tarda 3 segundos
+        await Task.Delay(3000);
         Console.WriteLine("Operación de larga duración A finalizada");
     }
 
     static async Task OperacionLargaDuracionB()
     {
         Console.WriteLine("Iniciando operación de larga duración...");
-        await Task.Delay(5000); // Simula una operación que tarda 5 segundos
+        await Task.Delay(5000);
         Console.WriteLine("Operación de larga duración B finalizada");
     }
 
     static async Task OperacionLargaDuracionC()
     {
         Console.WriteLine("Iniciando operación de larga duración...");
-        await Task.Delay(1000); // Simula una operación que tarda 1 segundos
+        await Task.Delay(1000);
         Console.WriteLine("Operación de larga duración C finalizada");
     }
 }
--- AsincroniaEscritorio/MainWindow.xaml.cs
+++ AsincroniaEscritorio/MainWindow.xaml.cs
@@ -29,15 +29,19 @@ namespace AsincroniaEscritorio
             WriteLine.TextAlignment = TextAlignment.Left;
             WriteLine.FontSize = 16;
             WriteLine.Text = "Bloqueo del Thread con Thread.Sleep...";
+            Consola.Items.Add($"[{DateTime.Now.ToString("HH:mm:ss")}] Bloqueo del Thread con Thread.Sleep...");
             await Task.Delay(1000);
             TareaLargaDuracion();
             WriteLine.Text = "Bloqueo finalizado.";
+            Consola.Items.Add($"[{DateTime.Now.ToString("HH:mm:ss")}] Bloqueo finalizado.");
             await Task.Delay(1000);
             WriteLine.Text = "Iniciando descarga de larga duración...";
+            Consola.Items.Add($"[{DateTime.Now.ToString("HH:mm:ss")}] Iniciando descarga de larga duración...");
             await Task.Delay(1000);
             await SimularDescarga("https://www.downloadmoreram.com");
             await Task.Delay(1000);
             WriteLine.Text = "Procesos finalizados.";
+            Consola.Items.Add($"[{DateTime.Now.ToString("HH:mm:ss")}] Procesos finalizados.");
         }
 
         public static void TareaLargaDuracion()
@@ -49,8 +53,10 @@ namespace AsincroniaEscritorio
         {
             await Task.Delay(1000);
             WriteLine.Text = $"Iniciando descarga desde \n{url}...";
+            Consola.Items.Add($"[{DateTime.Now.ToString("HH:mm:ss")}] Iniciando descarga desde {url}...");
             await Task.Delay(5000);
             WriteLine.Text = $"Descarga finalizada desde\n{url}.";
+            Consola.Items.Add($"[{DateTime.Now.ToString("HH:mm:ss")}] Descarga finalizada desde {url}.");
             await Task.Delay(1000);
         }
     }
